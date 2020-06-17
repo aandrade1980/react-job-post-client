77de6dc1f4ae171180ac55a82dfb66931d4ece6a
Alvaro <varito1@gmail.com>
2020-06-17 18:57:31 -0300

    Added tooltip for the user avatar

--- src/components/LoginAvatar.js
+++ src/components/LoginAvatar.js
@@ -1,31 +1,36 @@
-import React from "react";
+import React from 'react';
 
 // Redux
-import { connect } from "react-redux";
-import { logoutUser } from "../redux/actions/userActions";
+import { connect } from 'react-redux';
+import { logoutUser } from '../redux/actions/userActions';
+
+// Material UI
+import Tooltip from '@material-ui/core/Tooltip';
 
 const LoginAvatar = ({ user, logoutUser }) => {
   const handleClick = () => logoutUser();
 
   return (
-    <div
-      style={{ marginLeft: "auto", cursor: "pointer" }}
-      onClick={handleClick}
-    >
-      <img
-        style={styles.img}
-        src={user && user.avatar_url}
-        alt={user.full_name}
-      />
-    </div>
+    <Tooltip title={user.full_name}>
+      <div
+        style={{ marginLeft: 'auto', cursor: 'pointer' }}
+        onClick={handleClick}
+      >
+        <img
+          style={styles.img}
+          src={user && user.avatar_url}
+          alt={user.full_name}
+        />
+      </div>
+    </Tooltip>
   );
 };
 
 const styles = {
   img: {
     height: 42,
-    width: "auto",
-    borderRadius: "50%"
+    width: 'auto',
+    borderRadius: '50%'
   }
 };
 
@@ -33,7 +38,4 @@ const mapStateToProps = ({ user: { user } }) => ({
   user
 });
 
-export default connect(
-  mapStateToProps,
-  { logoutUser }
-)(LoginAvatar);
+export default connect(mapStateToProps, { logoutUser })(LoginAvatar);
